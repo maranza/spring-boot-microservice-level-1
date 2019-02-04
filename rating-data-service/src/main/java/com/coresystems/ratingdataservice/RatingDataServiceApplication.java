3fee24fa2fec934387eea22d4e1c67d29c29fabf
maranza <o.maranza@gmail.com>
2019-02-04 21:40:46 +0200

    initial commit

---
+++ rating-data-service/src/main/java/com/coresystems/ratingdataservice/RatingDataServiceApplication.java
@@ -0,0 +1,14 @@
+package com.coresystems.ratingdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingDataServiceApplication.class, args);
+	}
+
+}
+
