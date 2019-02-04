acba8c18cb3dd70934a522c9a157d9fd844015ee
maranza <o.maranza@gmail.com>
2019-02-05 00:08:36 +0200

    implement a connection between movie-catalog-service api and movie-info-service api

--- movie-catalog-service/src/main/java/com/coresystems/moviecatalogservice/controllers/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/coresystems/moviecatalogservice/controllers/MovieCatalogController.java
@@ -1,18 +1,37 @@
 package com.coresystems.moviecatalogservice.controllers;
 
 import com.coresystems.moviecatalogservice.models.CatalogItem;
+import com.coresystems.moviecatalogservice.models.Movie;
+import com.coresystems.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(new CatalogItem("Night School", "Kevin Hart in this movie shows us how school is important", 5));
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1", 4),
+                new Rating("2", 3)
+        );
+
+        return ratings.stream().map(rating ->{
+            //getting data from api endpoint and unmarshalling it into a local object
+            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie != null ? movie.getName() : "no movie found", "Kevin Hart in this movie shows us how school is important", rating.getRating());
+        }).collect(Collectors.toList());
+
+//        return Collections.singletonList(new CatalogItem("Night School", "Kevin Hart in this movie shows us how school is important", 5));
     }
 }
---
+++ movie-catalog-service/src/main/java/com/coresystems/moviecatalogservice/models/Rating.java
@@ -0,0 +1,30 @@
+package com.coresystems.moviecatalogservice.models;
+
+public class Rating {
+    private String movieId;
+    private Integer rating;
+
+    public Rating() {
+    }
+
+    public Rating(String movieId, Integer rating) {
+        this.movieId = movieId;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+}
