3c8127e04b920f6a427f59186fbb7bcb2b94c779
maranza <o.maranza@gmail.com>
2019-02-06 12:08:15 +0200

    configured movie-catalog-service to operate as a eureka service registry client

--- movie-catalog-service/src/main/java/com/coresystems/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/coresystems/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,15 @@ package com.coresystems.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
