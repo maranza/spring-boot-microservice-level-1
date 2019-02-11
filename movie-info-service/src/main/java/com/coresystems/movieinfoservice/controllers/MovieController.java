74c5a838ef1eab3d003df7813d381ad725d2e2a9
maranza <o.maranza@gmail.com>
2019-02-11 21:52:10 +0200

    removed unused imports

--- movie-info-service/src/main/java/com/coresystems/movieinfoservice/controllers/MovieController.java
+++ movie-info-service/src/main/java/com/coresystems/movieinfoservice/controllers/MovieController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
